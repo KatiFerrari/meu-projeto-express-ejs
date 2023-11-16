1280380399da4b0e0f2c53bd3ea4990d7f5319a1
KatiFerrari <katielidasilvaferrari26@gmail.com>
2023-11-16 16:44:18 -0400

    atualizado-16/11

--- app.js
+++ app.js
@@ -5,21 +5,36 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-const produtos = [
-  {id: 1, nome: "Definição Intensa", preco:"30", descricao: "Creme de pentear Salon Line 1000g", imagem: "produto1.png"},
-  {id: 2, nome: "Invigo Blonde Recharde Duo", preco:"150", descricao: "Kit Wella Professionais (2 produtos)", imagem: "produto2.png"},
-  {id: 3, nome: "#Todecacho Transição", preco:"18", descricao: "Gelatina Modeladora Salon Line", imagem: "produto3.png"},
-  {id: 4, nome: "Truss Curly Light", preco:"46", descricao: "Ativador de Cachos 250ml", imagem: "produto4.png"},
-  {id: 5, nome: "Siàge Cauterização dos Fios", preco:"218", descricao: "Combo Siàge Cauterização dos Fios (4 itens)", imagem: "produto5.png"},
-  {id: 6, nome: "Siàge Reconstrói Os Fios", preco:"142", descricao: "Kit Siàge Reconstrói Os Fios ( Shampoo + Cond. +máscara)", imagem: "produto6.png"},
-  {id: 7, nome: "Meu Cacho Minha Vida", preco:"60", descricao: "Kit Lola Cosmetics Meu Cacho Minha Vida Duo (2 Produtos)", imagem: "produto7.png"},
-  {id: 8, nome: "Be(M)dita Ghee", preco:"22", descricao: "Lola Cosmetics Be(M)dita Ghee Manteiga Vegetal Reconstrução 100g", imagem: "produto8.png"},
-  {id: 9, nome: "SOS Cachos", preco:"110", descricao: "Kit Salon Line para Cabelos Cacheados 3 a 4ABC", imagem: "produto9.png"},
-  {id: 10, nome: "#Todecacho", preco:"120", descricao: "Kit Salon Line #todecacho Babosa", imagem: "produto10.png"}
-]
+const produtos =[
+  {id: 1, nome: "Jogo PES 2019", preco:"509,39", descricao: "Jogo PES 2019 para PlayStation 4 P4DA0072101FGM", 
+  imagem: "produto1.png"},
+  {id: 2, nome: "Mario Strikers - Nintendo Switch", preco:"228,00", descricao: "PARTICIPE DE COPAS. Crienode in seu próprio torneio de eliminação dupla com até 4 jogadores no mesmo console e receba moedas no jogo.", 
+  imagem: "produto2.png"},
+  {id: 3, nome: "The Legend of Zelda - Tears of Kingdom", preco:"309,90", descricao: "Uma aventura épica pela terra e pelos céus de Hyrule aguarda em The Legend of Zelda™", 
+  imagem: "produto3.png"},
+  {id: 4, nome: "Breath Of The Wild - Nintendo Switch", preco:"289,89", descricao: "The Legend of Zelda: Breath of the Wild é a mais nova aventura da franquia, trazendo desta vez um mundo aberto vasto, rico e cheio de vida como nunca antes.", 
+  imagem: "produto4.png"},
+  {id: 5, nome: "Dungeons & Dragons: Kit Essencial", preco:"119,99", descricao: "Jogo PES 2019 para PlayStation 4 P4DA0072101FGM", 
+  imagem: "produto5.png"},
+  {id: 6, nome: "Dungeons & Dragons: Monster Manual", preco:"509,39", descricao: "Jogo PES 2019 para PlayStation 4 P4DA0072101FGM", 
+  imagem: "produto6.png"},
+  {id: 7, nome: "Jogo PES 2019", preco:"175,87", descricao: "O Livro dos Monstros reúne um verdadeiro acervo de criaturas icônicas presentes no universo de Dungeons & Dragons, incluindo dragões, gigantes, devoradores de mente e observadores. ", 
+  imagem: "produto7.png"},
+  {id: 8, nome: "Jogo PES 2019", preco:"509,39", descricao: "Jogo PES 2019 para PlayStation 4 P4DA0072101FGM", 
+  imagem: "produto8.png"},
+  {id: 9, nome: "Jogo PES 2019", preco:"509,39", descricao: "Jogo PES 2019 para PlayStation 4 P4DA0072101FGM", 
+  imagem: "produto9.png"},
+  {id: 10, nome: "Jogo PES 2019", preco:"509,39", descricao: "Jogo PES 2019 para PlayStation 4 P4DA0072101FGM", 
+  imagem: "produto10.png"}
+ ]
+
+ function  buscarProdutoPorID(id){
+  const produto = produtos.find(produto => produto.id == id);
+  return produto || null
+}
 
 app.get('/', (req, res) => {
-  res.render('index', { message: 'Olá, Mundo!' });
+  res.render('index', { produtos });
 });
 
 app.get('/produto1', (req, res) => {
